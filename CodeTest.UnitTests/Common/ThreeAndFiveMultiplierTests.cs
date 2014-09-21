aeb0a3b7572024062f954e4bf3cd2d46a0746d3f
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2014-09-22 00:11:33 +0300

    first task for ProjectEuler

---
+++ CodeTest.UnitTests/Common/ThreeAndFiveMultiplierTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace CodeTest.UnitTests.Common
+{
+    [TestFixture]
+    public class ThreeAndFiveMultiplierTests
+    {
+        [Test]
+        public void Test()
+        {
+            long result = 0;
+            for (long i = 3; i < 1000; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    Trace.WriteLine(i);
+                    result += i;
+                }
+            }
+            Trace.WriteLine(result);
+        }
+    }
+}
